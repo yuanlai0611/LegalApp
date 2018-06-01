ee8ba57534868e839cb85338289462cec659c460
yuanlaidada <994600653@qq.com>
2018-06-01 22:35:11 +0800

    add basic UI

---
+++ app/src/main/java/com/example/yuanyuanlai/legalapp/Activity/FacialRecognitionActivity.java
@@ -0,0 +1,101 @@
+package com.example.yuanyuanlai.legalapp.Activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.yuanyuanlai.legalapp.Base.BaseActivity;
+import com.example.yuanyuanlai.legalapp.R;
+
+public class CheckActivity extends BaseActivity implements BaseActivity.OpenCamera {
+
+    private final int TAKE_PHOTO_REQUEST = 1;
+    private Button takePhotoAgainButton;
+    private Button uploadButton;
+    private ImageView userImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        openCamera();
+        super.onCreate(savedInstanceState);
+
+    }
+
+    public static Intent newIntent(Context context){
+        Intent intent = new Intent(context,CheckActivity.class);
+        return intent;
+    }
+
+    @Override
+    public void setContentView() {
+        setContentView(R.layout.activity_check);
+    }
+
+    @Override
+    public void findViewById() {
+        takePhotoAgainButton = (Button)findViewById(R.id.takePhotoAgainButton);
+        uploadButton = (Button)findViewById(R.id.uploadButton);
+        userImageView = (ImageView)findViewById(R.id.user_picture);
+    }
+
+    @Override
+    public void initListener() {
+
+        takePhotoAgainButton.setOnClickListener(this);
+        uploadButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void askPermission() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.takePhotoAgainButton:
+                break;
+            case R.id.uploadButton:
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    @Override
+    public void openCamera(){
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,TAKE_PHOTO_REQUEST);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        switch (requestCode){
+
+            case TAKE_PHOTO_REQUEST:
+                if (requestCode == RESULT_CANCELED){
+                    Toast.makeText(this,"取消了拍照",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Bitmap photo = data.getParcelableExtra("data");
+                userImageView.setImageBitmap(photo);
+                break;
+            default:
+                break;
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/yuanyuanlai/legalapp/Guide/GuideActivity.java
@@ -0,0 +1,19 @@
+package com.example.yuanyuanlai.legalapp;
+
+import android.os.Bundle;
+
+import com.example.yuanyuanlai.legalapp.Base.BaseActivity;
+
+public class GuideActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+
+
+
+    }
+
+
+}
